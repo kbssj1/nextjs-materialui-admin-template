e8b3d990033196089341d021814bd20d4c52588f
‘kbssj1’ <>
2024-03-04 22:01:33 +0900

    add form

--- src/app/system/form/page.tsx
+++ src/app/system/form/page.tsx
@@ -1,12 +1,80 @@
 'use client';
 
 import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
 export default function Page() {
 
   return (
-    <div>
-      form
-    </div>
+    <Box width={600}>
+      
+      <Box component="section" sx={{ p: 2 }}>
+
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link 
+            underline="hover" 
+            color="inherit" 
+            href="/system">
+            System1
+          </Link>
+          <Typography color="text.primary">Create</Typography>
+        </Breadcrumbs>
+
+      </Box>
+
+
+      <Grid container spacing={2}>
+        <Grid item >
+          <TextField
+            id="id"
+            label="id"
+            type="String"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+
+        <Grid item >
+          <TextField
+            id="productid"
+            label="productId"
+            type="String"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+
+        <Grid item >
+          <TextField
+            id="customerId"
+            label="customerId"
+            type="String"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+
+        <Grid item >
+          <TextField
+            id="transactionDate"
+            label="transactionDate"
+            type="Date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+
+      </Grid>
+    </Box>
+
   );
 }
