addabe4b21d182e3afaa6fe54381441fc8b28d75
kbssj1 <kbssj1@naver.com>
2024-03-08 22:03:07 +0900

    dashboard

--- src/app/dashboard/dashboard1/page.tsx
+++ src/app/dashboard/dashboard1/page.tsx
@@ -1,9 +1,93 @@
+"use client";
+
 import React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 export default function Page() {
-  return (
-     <div>
-        dashboard1
-     </div>
-  );
+   return (
+      <Box
+         width={1000}
+         display="flex"
+         alignItems="center"
+         gap={4}
+         p={2}
+      >
+         <Grid container spacing={2}>
+            <Grid item xs={6}>
+               <BarChart
+                  xAxis={[
+                     {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                        scaleType: 'band',
+                     },
+                  ]}
+                  series={[
+                     {
+                        data: [2, 5, 3],
+                     },
+                  ]}
+                  width={500}
+                  height={300}
+               />
+            </Grid>
+            <Grid item xs={6}>
+               <BarChart
+                  xAxis={[
+                     {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                        scaleType: 'band',
+                     },
+                  ]}
+                  series={[
+                     {
+                        data: [2, 5, 3],
+                     },
+                  ]}
+                  width={500}
+                  height={300}
+               />
+            </Grid>
+            <Grid item xs={6}>
+               <BarChart
+                  xAxis={[
+                     {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                        scaleType: 'band',
+                     },
+                  ]}
+                  series={[
+                     {
+                        data: [2, 5, 3],
+                     },
+                  ]}
+                  width={500}
+                  height={300}
+               />
+            </Grid>
+            <Grid item xs={6}>
+               <BarChart
+                  xAxis={[
+                     {
+                        id: 'barCategories',
+                        data: ['bar A', 'bar B', 'bar C'],
+                        scaleType: 'band',
+                     },
+                  ]}
+                  series={[
+                     {
+                        data: [2, 5, 3],
+                     },
+                  ]}
+                  width={500}
+                  height={300}
+               />
+            </Grid>
+         </Grid>
+      </Box>
+    );
 }
