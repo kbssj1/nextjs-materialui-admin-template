917196319b36306d3fb50eae082b1a4c460dc180
kbssj1 <kbssj1@naver.com>
2024-03-10 22:03:14 +0900

    update dashbaord

--- src/app/dashboard/dashboard1/page.tsx
+++ src/app/dashboard/dashboard1/page.tsx
@@ -4,8 +4,20 @@ import React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Transactions from '../../mockData/transactionData.json';
 
 export default function Page() {
+
+   function calculateCount(arr:any[], standard:string) {
+      let result:any[] = arr.reduce((prev, curr) => (prev[curr[standard]] = ++prev[curr[standard]] || 1, prev), {});
+      return Object.values(result);
+   }
+
+   const data = Transactions.transactions;
+   const xAxis = Array.from(new Set(data.map((d) => d.productid)));
+   const series = calculateCount(data, "productid");
+   
    return (
       <Box
          width={1000}
@@ -16,6 +28,30 @@ export default function Page() {
       >
          <Grid container spacing={2}>
             <Grid item xs={6}>
+               <Typography variant="h5">
+                  Dashboard1
+               </Typography>
+               <BarChart
+                  xAxis={[
+                     {
+                        id: 'barCategories',
+                        data: xAxis,
+                        scaleType: 'band',
+                     },
+                  ]}
+                  series={[
+                     {
+                        data: series,
+                     },
+                  ]}
+                  width={500}
+                  height={300}
+               />
+            </Grid>
+            <Grid item xs={6}>
+               <Typography variant="h5">
+                  Dashboard1
+               </Typography>
                <BarChart
                   xAxis={[
                      {
@@ -34,6 +70,9 @@ export default function Page() {
                />
             </Grid>
             <Grid item xs={6}>
+               <Typography variant="h5">
+                  Dashboard1
+               </Typography>
                <BarChart
                   xAxis={[
                      {
@@ -52,24 +91,9 @@ export default function Page() {
                />
             </Grid>
             <Grid item xs={6}>
-               <BarChart
-                  xAxis={[
-                     {
-                        id: 'barCategories',
-                        data: ['bar A', 'bar B', 'bar C'],
-                        scaleType: 'band',
-                     },
-                  ]}
-                  series={[
-                     {
-                        data: [2, 5, 3],
-                     },
-                  ]}
-                  width={500}
-                  height={300}
-               />
-            </Grid>
-            <Grid item xs={6}>
+               <Typography variant="h5">
+                  Dashboard1
+               </Typography>
                <BarChart
                   xAxis={[
                      {
