e9be96eb7fec83f4b567ab7afeb4c828926aa41f
kbssj1 <kbssj1@naver.com>
2024-05-22 11:09:15 +0900

    add tab page

--- src/app/system/system1/page.tsx
+++ src/app/system/system1/page.tsx
@@ -10,12 +10,40 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default function Page() {
   const data = Transactions.transactions;
   const [selectedItem, setSelectedItem] = React.useState<GridRowSelectionModel>([]);
   const [disabledDeleteButton, setDisabledDeleteButton] = React.useState(true);
   const [disabledUpdateButton, setDisabledUpdateButton] = React.useState(true);
+  const [tabValue, setTabValue] = React.useState(0);
   const router = useRouter();
 
   React.useEffect(() => {
@@ -54,6 +82,10 @@ export default function Page() {
       setDisabledUpdateButton(true);
     }
   }
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
   return (
     <div id="systemPage1" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
@@ -94,18 +126,30 @@ export default function Page() {
       </div>
       {!disabledUpdateButton && 
       <Box sx={{ border: '1px solid grey' }}>
-        <Stack direction="row" spacing={2} padding={2}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom>
-                  Transactions
-                </Typography>
-                <Typography variant="h5">
-                  {data.length}
-                </Typography>
-              </CardContent>
-            </Card>
-        </Stack>
+        <Tabs
+          value={tabValue}
+          onChange={handleTabChange}
+        >
+          <Tab label="Statistics" />
+          <Tab label="Detail" />
+        </Tabs>
+        <TabPanel value={tabValue} index={0}>
+          <Stack direction="row" spacing={2} padding={2}>
+              <Card>
+                <CardContent>
+                  <Typography gutterBottom>
+                    Transactions
+                  </Typography>
+                  <Typography variant="h5">
+                    {data.length}
+                  </Typography>
+                </CardContent>
+              </Card>
+          </Stack>
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          Detail
+        </TabPanel>
       </Box>}
     </div>
 
